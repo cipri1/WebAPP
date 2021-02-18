b64d6ebaa09edf5a4d056c835ed4f0c66226b1a0
CipriRaul <cipri167@gmail.com>
2021-02-18 20:04:51 +0200

    proiect final

---
+++ Data/Samarghitean_Ciprian_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Samarghitean_Ciprian_Lab8.Models;
+
+namespace Samarghitean_Ciprian_Lab8.Data
+{
+    public class Samarghitean_Ciprian_Lab8Context : DbContext
+    {
+        public Samarghitean_Ciprian_Lab8Context(DbContextOptions<Samarghitean_Ciprian_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Samarghitean_Ciprian_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Samarghitean_Ciprian_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Samarghitean_Ciprian_Lab8.Models.Category> Category { get; set; }
+    }
+}
---
+++ Migrations/20210218171649_redo.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Samarghitean_Ciprian_Lab8.Migrations
+{
+    public partial class redo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Book",
+                maxLength: 150,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Author",
+                table: "Book",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Book",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 150);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Author",
+                table: "Book",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 50);
+        }
+    }
+}
